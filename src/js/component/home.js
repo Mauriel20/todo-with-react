08b566bfd69e7072fdc94b29f198784c394a4330
Anmora96 <anmora.2396@gmail.com>
2021-02-10 23:06:14 +0000

    Investigar elimiar elemento por medio de fliter e id

--- src/js/component/home.js
+++ src/js/component/home.js
@@ -1,24 +1,65 @@
 import React from "react";
-
+import shortid from "shortid";
 //include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
-
 //create your first component
 export function Home() {
+	const [tarea, setTarea] = React.useState("");
+	const [arrayTareas, setArrayTareas] = React.useState([]);
+	const agregarTarea = e => {
+		e.preventDefault();
+		console.log(tarea);
+		setArrayTareas([
+			...arrayTareas,
+			{
+				id: shortid.generate(),
+				nombreTarea: tarea
+			}
+		]);
+
+		setTarea("");
+	};
+
+	upDateTask = id => {
+		const updatedTasks = this.state.tarea.filter(
+			tarea => tarea != this.state.tarea[id]
+		);
+		this.setState({ tarea: updatedTasks });
+	};
+
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
+		<div className="container">
+			<h1 className="text-center">CRUD de Tareas con React</h1>
+			<div className="row">
+				<div className="col-12">
+					<h4 className="text-center">Lista de Tareas</h4>
+					<ul className="list-group">
+						<form onSubmit={agregarTarea}>
+							<li className="list-group-item">
+								<input
+									type="text"
+									className="form-control mb-2"
+									placeholder="Ingrese Tarea"
+									onChange={e => setTarea(e.target.value)}
+									value={tarea}
+								/>
+							</li>
+						</form>
+						{arrayTareas.map(item => (
+							<li className="list-group-item" key={item.id}>
+								<span className="lead">{item.nombreTarea}</span>
+								<button
+									type="button"
+									className="ml-2 mb-1 close"
+									data-dismiss="toast"
+									aria-label="Close"
+									onClick={upDateTask}>
+									<span aria-hidden="true">&times;</span>
+								</button>
+							</li>
+						))}
+					</ul>
+				</div>
+			</div>
 		</div>
 	);
 }
